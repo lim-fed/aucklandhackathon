a2d24a069f7cb29a25a199f52d811f0cce9a63b4
Liam Coll <liam.coll@accenture.com>
2023-08-10 15:42:57 +1200

    changing times

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -15,35 +15,35 @@ const chats = [
     sentFromUser: false,
   },
   {
-    time: "10:33pm",
+    time: "10:34pm",
     message: "Sure! They started flowering yesterday.",
     sentFromUser: true,
   },
   {
-    time: "10:33pm",
+    time: "10:36pm",
     image: "./tomato.png",
     sentFromUser: true,
   },
   {
-    time: "10:33pm",
+    time: "10:36pm",
     message:
       "They are looking great! Give the plant one cup of water today and leave it in the sun.",
     sentFromUser: false,
   },
   {
-    time: "10:33pm",
+    time: "10:36pm",
     message:
       "These should be ready to eat in four weeks time. Would you like me to schedule a Market Place post for your excess tomatoes?",
     sentFromUser: false,
   },
   {
-    time: "10:33pm",
+    time: "10:37pm",
     message:
       "Yes please!! I would love to swap my excess tomatoes for some fresh basil :)",
     sentFromUser: true,
   },
   {
-    time: "10:33pm",
+    time: "10:37pm",
     message:
       "Great! I have scheduled your Market Place post for August 31st. This can be edited or removed any time via your profile.",
     sentFromUser: false,
